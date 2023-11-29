eebc3ac83f56e3b18810e12a075a48b9ce506e5d
Aishatu Salisu Suleiman <ayshertsalis003@gmail.com>
2023-11-29 05:04:29 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function home() {
+    location.reload("..index.html")
+}
+function aboutMe() {
+    location.replace("../about.html")
+}
+function contactMe() {
+    location.replace("../contact.html")
+}
+function myWork() {
+    location.replace("../work.html")
+}
+function hiddenMenu() {
+    document.getElementsByClassName("menu").style.display = "none";
+}
